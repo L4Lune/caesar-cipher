92af8d2e5f04e5be4e1e11f3c33e28bf413c389b
Jack Roudebush <roudebushjack@gmail.com>
2023-04-12 22:04:13 -0400

    Complete Caesar Cipher exercise

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,40 @@
-require 'pry-byebug'
 def caesar_cipher(phrase, shift_factor)
-    alpha = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
-    indexed_alpha = alpha.map.with_index {|ch, idx| [ch, idx]}
-    phrase.split('')
-    shifted_alpha_index = indexed_alpha.pop(shift_factor).map.with_index {|ch, idx| [ch, idx]}
-    puts shifted_alpha_index
-    # binding.pry
-end
+    split_phrase = phrase.split('')
+    ord_phrase_array = []
+    shift_array = []
+    caesar_ord_array = []
+    tranlated_phrase = ''
+    
+    # Get ord numbers for the phrase
+    split_phrase.each do |item|
+        value = item.ord
+        ord_phrase_array.push(value)
+    end
 
-# binding.pry
-caesar_cipher('hello',3)
-# binding.pry
+    # Shift the ord numbers to the shift_factor
+    ord_phrase_array.each do |item|
+        if ((item + shift_factor).between?(65, 90)) || ((item + shift_factor).between?(97, 122))
+            shift_array.push(item + shift_factor)
+        elsif (item + shift_factor).between?(91,96)
+            shift_array.push((item - 26) + shift_factor)
+        elsif (item + shift_factor) > 123
+            shift_array.push((item - 26) + shift_factor)
+        else
+            shift_array.push(item)
+        end
+    end
+
+    #Translate back to ascii
+    shift_array.each do |item|
+        caesar_ord_array.push(item.chr)
+    end
+
+    #Join the array
+    translated_phrase = caesar_ord_array.join("")
+
+    p ord_phrase_array
+    p shift_array
+    p caesar_ord_array
+    p translated_phrase
+end
+caesar_cipher("wxyz", 2)
